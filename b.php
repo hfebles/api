e5ab6c9a01544492415497382a5ad5ce7ee30b4f
Jesus Hernandez <jehfebles@gmail.com>
2020-04-24 11:21:48 -0400

    1.0.6

--- b.php
+++ b.php
@@ -13,41 +13,24 @@ $h = array(
       'Accept: application/json'
    );
 
-
-$setUp = array(
-         "CURLOPT_POST" => true,
-         "CURLOPT_URL" => $u,
-         "CURLOPT_HEADER" => true,
-         "CURLOPT_HTTPHEADER" => $h
-
-         );
-
-//echo $u.$t;
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_URL, $u);
 curl_setopt($curl, CURLOPT_HEADER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, );
+curl_setopt($curl, CURLOPT_HTTPHEADER, $h);
 
-var_dump($setUp);
 
-/*
 
-   $curl = curl_init();
 
-   curl_setopt($curl, CURLOPT_POST, true);
-   curl_setopt($curl, CURLOPT_URL, $url);
-   curl_setopt($curl, CURLOPT_HEADER, true);
-   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'Authorization: Bearer 8WCDVcfOM8upIQi0Sgl3olWUF3B5rwi5lQnS9D31vvmJTI1bABKwt66j6LGATka7k2RB8Ire5V2cfpMc',
-      'Content-Type: application/json',
-      'Accept: application/json'
-   ));
+
+
+
+
 
    curl_setopt($curl, CURLOPT_POSTFIELDS, 'traking=200204315204');
 
    //curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
    // EXECUTE:
-   //$result = curl_exec($curl);
+   $result = curl_exec($curl);
 
 
    /*if(!$result){die("Connection Failure");}
