01dbf3728fb29f2352f475b005d5675959440611
Jesus Hernandez <jehfebles@gmail.com>
2020-04-24 11:37:01 -0400

    1.0.7

--- b.php
+++ b.php
@@ -1,6 +1,6 @@
 <?php
 
-$u = 'apostilla.desarrollo.intt.gob.ve/api/consular-letter/apostilled';
+$u = 'http://192.168.106.102/api/consular-letter/apostilled';
 $t = '8WCDVcfOM8upIQi0Sgl3olWUF3B5rwi5lQnS9D31vvmJTI1bABKwt66j6LGATka7k2RB8Ire5V2cfpMc';
 
 $curl = curl_init();
@@ -13,10 +13,10 @@ $h = array(
       'Accept: application/json'
    );
 
-curl_setopt($curl, CURLOPT_POST, true);
+//curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_URL, $u);
-curl_setopt($curl, CURLOPT_HEADER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $h);
+//curl_setopt($curl, CURLOPT_HEADER, true);
+//curl_setopt($curl, CURLOPT_HTTPHEADER, $h);
 
 
 
@@ -26,7 +26,7 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, $h);
 
 
 
-   curl_setopt($curl, CURLOPT_POSTFIELDS, 'traking=200204315204');
+  // curl_setopt($curl, CURLOPT_POSTFIELDS, 'traking=200204315204');
 
    //curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
    // EXECUTE:
@@ -38,7 +38,7 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, $h);
    return $result;*/
 
 
-   //var_dump(curl_setopt());
+   var_dump($result);
 
 
 
